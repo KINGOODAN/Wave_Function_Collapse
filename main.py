54387039a75d5933d145fea31b989b8fa70f4738
KINGOODAN <aidanlhbaros@gmail.com>
2023-04-06 14:45:45 -0600

    put back debug

--- main.py
+++ main.py
@@ -11,10 +11,11 @@ class Game:
         self.Running = True
         self.grid:list[list[Slot]] = []
         self.done:bool = False
+        self.tile_size:int = 60
+        self.img_tile_dimentions:tuple[int,int] = (4,4)
 
     def run(self):
-        
-        self.grid = makeGrid(self.screen, self.grid)
+        self.grid = makeGrid(self.screen, self.grid, self.tile_size, self.img_tile_dimentions)
         while self.Running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -23,17 +24,16 @@ class Game:
             if keys[pygame.K_LCTRL]:
                 self.Running = False
             if keys[pygame.K_LSHIFT]:
-                self.grid = makeGrid(self.screen, self.grid)
+                self.grid = makeGrid(self.screen, self.grid, self.tile_size, self.img_tile_dimentions)
 
             self.screen.fill((0,0,0))
 
             if not self.done: #type: ignore
                 self.done,self.grid = determine_possibilities(self.grid)
-                self.grid = collapse((4,4), self.grid)
+                self.grid = collapse(self.img_tile_dimentions, self.grid)
 
-            scale = 54
-            for i in range(int(self.screen.get_height()/scale)):
-                for j in range(int(self.screen.get_width()/scale)):
+            for i in range(int(self.screen.get_height()/self.tile_size)):
+                for j in range(int(self.screen.get_width()/self.tile_size)):
                     self.grid[i][j].draw()
 
             time = self.clock.tick() / 1000
--- tile.py
+++ tile.py
@@ -1,7 +1,6 @@
 import pygame
 import random
 import copy
-import numpy as np
 
 
 
@@ -21,8 +20,8 @@ class Tile:
         ]
         for i in range(4):
             self.sides.append(image.unmap_rgb(self.pixel[self.check_positions[i]])) #type: ignore
-            #print(image.unmap_rgb(self.pixel[self.check_positions[i]]),end=", ") #type: ignore
-            #print(self.check_positions[i])
+            print(image.unmap_rgb(self.pixel[self.check_positions[i]]),end=", ") #type: ignore
+            print(self.check_positions[i])
 
 class Slot:
     def __init__(self, pos: tuple[int,int], scale: int, size: tuple[int,int]): #scale is the multiplier the size is how many tile x then y
@@ -37,26 +36,30 @@ class Slot:
         self.entropy = self.size[0]*self.size[1]
         self.template = pygame.image.load("test_image.png")
         self.pixel = None
+        self.base_tile_size:tuple[int,int] = (100,100)
+        self.tile_size:int = 60
 
         for i in range(self.size[0]):
             for j in range(self.size[1]):
-                self.possibilities.append(Tile(self.template,i,j,(100,100))) 
+                self.possibilities.append(Tile(self.template,i,j,self.base_tile_size)) 
 
     def draw(self):
         if self.collapsed:
-            self.tile.image = pygame.transform.scale(self.template, (216, 216))
-            self.screen.blit(self.tile.image, self.pos,(54*self.tile.stepx,54*self.tile.stepy,54,54),)
+            self.tile.image = pygame.transform.scale(self.template, (self.tile_size*4, self.tile_size*4))
+            self.screen.blit(self.tile.image, self.pos,(self.tile_size*self.tile.stepx,self.tile_size*self.tile.stepy,self.tile_size,self.tile_size),)
             
         
     def collapse(self):
-        #print("test")
         if len(self.possibilities) != 0:
-            print("______test")
             self.collapsed = True
             randomSlot = random.randint(0,len(self.possibilities)-1)
             self.tile = self.possibilities[randomSlot]
-            
-        
+
+#PUT BACK DEBUG
+#PUT BACK DEBUG
+#PUT BACK DEBUG
+#PUT BACK DEBUG
+#PUT BACK DEBUG
 
 
 def determine_possibilities(grid:list[list[Slot]]):
@@ -91,6 +94,7 @@ def determine_possibilities(grid:list[list[Slot]]):
             t.entropy = len(t.possibilities)
     return isDone, grid
 
+
 def collapse(size:tuple[int,int], grid:list[list[Slot]]):
     lowestEntropy = size[0]*size[1]
     lowestList:list[Slot] = []
@@ -111,14 +115,12 @@ def collapse(size:tuple[int,int], grid:list[list[Slot]]):
     return grid
 
 
-
-def makeGrid(screen:pygame.surface.Surface, grid:list[list[Slot]]):
+def makeGrid(screen:pygame.surface.Surface, grid:list[list[Slot]], tile_size:int, img_tile_dimentions):
     grid = []
-    scale = 54
-    for i in range(int(screen.get_height()/scale)):
+    for i in range(int(screen.get_height()/tile_size)):
         grid.append([])
-        for j in range(int(screen.get_width()/scale)):
-            grid[i].append(Slot((j*scale,i*scale), scale, (4,4)))
+        for j in range(int(screen.get_width()/tile_size)):
+            grid[i].append(Slot((j*tile_size,i*tile_size), tile_size, img_tile_dimentions))
     return grid
 
 
